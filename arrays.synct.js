f164855a8b2f75208c396b5f18f8b4bb1ba4b8be
Dominic Tarr <dominic.tarr@gmail.com>
2011-08-15 17:49:05 +1000

    initial

---
+++ arrays.synct.js
@@ -0,0 +1,48 @@
+
+var arrays = require('d-utils/arrays')
+  , it = require('it-is')
+
+exports ['api'] = function (){
+
+  it(arrays).has({init: it.function (),last: it.function (),head: it.function (),tail: it.function ()})
+
+}
+
+exports ['init, head, tail, last'] = function (){
+
+var x = [1,2,3,4,5,6,7,8,9,0]
+
+  it(arrays.head(x)).deepEqual(1)
+  it(arrays.last(x)).deepEqual(0)
+
+  it(arrays.init(x)).deepEqual([1,2,3,4,5,6,7,8,9])
+  it(arrays.tail(x)).deepEqual([2,3,4,5,6,7,8,9,0])
+
+  it(x).deepEqual([1,2,3,4,5,6,7,8,9,0]) //no not mutate the array
+}
+
+exports ['empty list behaviour strict'] = function (){
+
+var x = []
+
+  it(function () {arrays.strictHead(x) }).throws()
+  it(function () {arrays.strictLast(x) }).throws()
+  it(function () {arrays.strictInit(x) }).throws()
+  it(function () {arrays.strictTail(x) }).throws()
+
+}
+exports ['empty list behaviour easy'] = function (){
+
+var x = []
+
+  it(arrays.head(x)).equal(null)
+  it(arrays.last(x)).equal(null)
+  it(arrays.tail(x)).deepEqual([])
+  it(arrays.init(x)).deepEqual([])
+
+}
+
+exports ['empty'] = function (){
+  it(arrays.empty([])).equal(true)
+  it(arrays.empty([1])).equal(false)
+}
