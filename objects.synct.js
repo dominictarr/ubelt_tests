f41d32aa0d3f2e21401b97593062ccf0357c901c
Dominic Tarr <dominic.tarr@gmail.com>
2011-08-22 13:15:29 +1000

    get carried away with mapping on numbers like in haskell

--- objects.synct.js
+++ objects.synct.js
@@ -18,15 +18,78 @@ exports ['map'] = function (){
   var on = {a: 1,b: 2, c: 3}, count = 0
   var off = 
     objects.map(on, function(v,k){
-    it(v).equal(on[k])
-    count ++
-    return v * 2
-  })
+      it(v).equal(on[k])
+      count ++
+      return v * 2
+    })
   it(count).equal(3)
   it(off).deepEqual({
     a: 2, b: 4,c: 6
   })
 }
+
+exports ['map -- {}'] = function (){
+
+  var on = {}, count = 0
+  var off = 
+    objects.map(on, function(v,k){
+      count ++
+    })
+  it(count).equal(0)
+  it(off).deepEqual({})
+}
+
+exports ['map -- {}'] = function (){
+
+  var on = {}, count = 0
+  var off = 
+    objects.map(on, function(v,k){
+      count ++
+    })
+  it(count).equal(0)
+  it(off).deepEqual({})
+}
+
+exports ['map on numbers -- start at 1'] = function () {
+  
+  var seven = objects.map(7, function (n) {
+      return n
+  })
+
+  it(seven).deepEqual([1,2,3,4,5,6,7])
+  
+}
+
+exports ['map on numbers: 0 - 8'] = function () {
+  
+  var seven = objects.map(0, 8, function (n) {
+      return n
+  })
+
+  it(seven).deepEqual([0,1,2,3,4,5,6,7,8])
+
+}
+
+exports ['map even numbers: 2,4...12'] = function () {
+  
+  var seven = objects.map(2, 4, 12, function (n) {
+      return n
+  })
+
+  it(seven).deepEqual([2,4,6,8,10,12])
+}
+
+exports ['map even numbers: 12, 10...2 in reverse'] = function () {
+  
+  var seven = objects.map(12, 10, 2, function (n) {
+      return n
+  })
+
+  it(seven).deepEqual([2,4,6,8,10,12].reverse())
+
+}
+
+
 exports ['filter'] = function (){
   
   var on = {a: 1,b: 2, c: 3, d:4, e:5, f:6, g:7}, count = 0
