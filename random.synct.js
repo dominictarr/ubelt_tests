4a9740d3a6ef847a136893551ec161bca01b9ebd
Dominic Tarr <dominic.tarr@gmail.com>
2011-11-30 23:19:03 +1100

    dumb mistakes mean you need to test seemingly trivial things.

--- random.synct.js
+++ random.synct.js
@@ -14,3 +14,45 @@ exports['integer'] = function () {
   }
 
 }
+
+exports['0, 1 inclusive'] = function () {
+
+  var l = 1000
+    , zero = false
+    , one = false
+  while (l--) {
+  
+    var n = r.randomInt(0, 1)
+    if(n == 0)
+      zero = true
+    if(n == 1)
+      one = true    
+    it(n).betweenOrEqual(0, 1)
+  }
+  it(one).ok()
+  it(zero).ok()
+}
+
+exports['arrays 1'] = function () {
+
+  var l = 10
+  var ex = ['x']
+  
+  while (l--) {
+    var n = r.randomOf(ex)
+    it(n).equal('x')
+  }
+
+}
+
+exports['arrays 2'] = function () {
+
+  var l = 10
+  var ex = ['x', 'y']
+  
+  while (l--) {
+    var n = r.randomOf(ex)
+    it(n).matches(/x|y/)
+  }
+
+}
